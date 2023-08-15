6c5019f307763a42138b138806555e3c2e80d604
Samuelolushegun <etchoudinansamuel@gmail.com>
2023-08-15 15:27:37 +0000

    Add Advanced Task and others Updates

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listobject.h"
+#include "object.h"
+#include "lists.h"
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: pointer to a python oblect
+ *
+ * Return: Nothing
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int taille = PyList_Size(p);
+
+	printf("[*] Size of Python List = %d\n", taille);
+	printf("[*] Allocated = %d\n", PyList_GET_SIZE(p));
+	if (taille > 0)
+	{
+		for (i = 0 ; i < taille; i++)
+			printf("Element %d: %p\n", Py_TYPE(PyList_GetItem(p, i))
+				);
+	}
+}
