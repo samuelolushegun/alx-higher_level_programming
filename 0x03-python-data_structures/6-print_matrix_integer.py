9873a70f0f21254c6dfc5f4b6b9c72e0a8771297
Samuelolushegun <etchoudinansamuel@gmail.com>
2023-08-15 09:02:41 +0000

    Update Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
         for liste in matrix:
             idx_elmt = len(liste)
             for i in range(0, idx_elmt):
-                print("{}".format(liste[i]), end="")
+                print("{:d}".format(liste[i]), end="")
                 if i != idx_elmt - 1:
                     print(" ", end="")
                 else:
