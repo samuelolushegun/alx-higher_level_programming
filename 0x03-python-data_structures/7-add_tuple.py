cfd8d31ea0deb3c521fdbb2ce038dc665234454a
Samuelolushegun <etchoudinansamuel@gmail.com>
2023-08-15 06:45:29 +0000

    Add Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    n_tuple = (0, 0)
+    if len(tuple_a) >= 1:
+        a1 = tuple_a[0]
+    else:
+        a1 = 0
+    if len(tuple_a) > 1:
+        a2 = tuple_a[1]
+    else:
+        a2 = 0
+    if len(tuple_b) >= 1:
+        b1 = tuple_b[0]
+    else:
+        b1 = 0
+    if len(tuple_b) > 1:
+        b2 = tuple_b[1]
+    else:
+        b2 = 0
+    n_tuple = (a1 + b1, a2 + b2)
+    return n_tuple
