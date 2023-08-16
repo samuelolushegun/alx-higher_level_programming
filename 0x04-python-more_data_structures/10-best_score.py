3128ae087e0fe91a11a421fce34f80789994918b
Samuelolushegun <etchoudinansamuel@gmail.com>
2023-08-16 18:08:09 +0000

    Add Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        for key in a_dictionary:
+            b_val = a_dictionary[key]
+            break
+        for key, value in a_dictionary.items():
+            if value > b_val:
+                b_val = value
+                b_key = key
+        return b_key
