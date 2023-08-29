9d81ca7e81ec95f66593d2eae7e6516d38731547
Samuel <etchoudinansamuel@gmail.com>
2023-08-29 17:21:12 +0000

    Add Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module write an empty class Square that defines a square"""
+
+
+class Square:
+    """Simple class of a square"""
+    def __init__(self, size):
+        """Initialize the square.
+
+        Arg:
+            size: size of the square.
+    """
+        self._size = size
