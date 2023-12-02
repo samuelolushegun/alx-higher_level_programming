ea1f50081c5bd048f2791c98b76a14c282e787c3
Samuel <etchoudinansamuel@gmail.com>
2023-12-02 01:52:21 +0000

    Update in task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -22,8 +22,8 @@ def matrix_divided(matrix, div):
                         if isinstance(j, (int, float)):
                             nlist.append(round((j / div), 2))
                         else:
-                            raise TypeError("matrix must be a matrix (list"
-                                            "of lists) of integers/floats")
+                            raise TypeError("matrix must be a matrix (array"
+                                            "of arrays) of integers/floats")
                     new_mat.append(nlist)
                 else:
                     raise TypeError("Each row of the matrix must have the "
