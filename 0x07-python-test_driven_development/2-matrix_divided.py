5a91083ec22c3c20edcff37d2fbf3e7bd54a66b9
Samuel <etchoudinansamuel@gmail.com>
2023-12-02 01:16:40 +0000

    Add Task 2 files

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+This module is for divided matrix function
+'''
+
+
+def matrix_divided(matrix, div):
+    '''
+    divide each element for a matrix by argument div
+    '''
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if isinstance(matrix, list):
+        new_mat = []
+        for i in matrix:
+            if isinstance(i, list):
+                a = len(matrix[0])
+                if len(i) == a:
+                    nlist = []
+                    for j in i:
+                        if isinstance(j, (int, float)):
+                            nlist.append(round((j / div), 2))
+                        else:
+                            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                    new_mat.append(nlist)
+                else:
+                    raise TypeError("Each row of the matrix must have the same size")
+            else:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    else:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    return (new_mat)
