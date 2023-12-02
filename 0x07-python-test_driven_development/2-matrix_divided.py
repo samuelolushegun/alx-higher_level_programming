ecb1b6db7b149dec27d1c724dcb8154205dee9f1
Samuel <etchoudinansamuel@gmail.com>
2023-12-02 01:35:38 +0000

    PEP8 requirement fixing

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -22,12 +22,16 @@ def matrix_divided(matrix, div):
                         if isinstance(j, (int, float)):
                             nlist.append(round((j / div), 2))
                         else:
-                            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                            raise TypeError("matrix must be a matrix (list"
+                                            "of lists) of integers/floats")
                     new_mat.append(nlist)
                 else:
-                    raise TypeError("Each row of the matrix must have the same size")
+                    raise TypeError("Each row of the matrix must have the "
+                                    "same size")
             else:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of"
+                                "integers/floats")
     else:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
     return (new_mat)
