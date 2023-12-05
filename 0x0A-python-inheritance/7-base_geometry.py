4ac2232866812fb2f75d9df47457873a2aed7eb8
Samuel <etchoudinansamuel@gmail.com>
2023-12-05 01:10:07 +0000

    Add Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Geometry module'''
+
+
+class BaseGeometry:
+    '''My Objecct BaseGeometry'''
+
+    def area(self):
+        '''Method that raised an Exception for the moment'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''Method that validates value'''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
