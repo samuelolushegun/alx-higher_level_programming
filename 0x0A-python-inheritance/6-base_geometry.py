f0a147e0afa1f87e6241ef5d54d2192777a51103
Samuel <etchoudinansamuel@gmail.com>
2023-12-05 00:05:01 +0000

    Add Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Geometry module'''
+
+
+class BaseGeometry:
+    '''My Objecct BaseGeometry'''
+
+    def area(self):
+        '''Method that raised an Exception for the moment'''
+        raise Exception("area() is not implemented")
