1a7cba350a45107d3c061d02a888c0cf56094527
Samuel <etchoudinansamuel@gmail.com>
2023-12-06 00:39:22 +0000

    Add Task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module to repr From JSON string to Object'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    This function returns an object (Python data structure)
+    represented by a JSON string:
+    '''
+    return (json.loads(my_str))
