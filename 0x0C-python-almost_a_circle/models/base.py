a54ce97844650930b0e747b57e7b7b7bf74894cd
Samuel <etchoudinansamuel@gmail.com>
2023-12-12 21:56:19 +0000

    Update Base module for test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,6 +46,6 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        dummy = cls(1, 2, 3)
+        dummy = cls(1, 2)
         dummy.update(**dictionary)
         return (dummy)
