811cb0f43aa9a0564ba3dbb70a0ea61981e4a975
Samuel <etchoudinansamuel@gmail.com>
2023-12-12 21:52:45 +0000

    Update base module

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,9 @@ class Base:
             return ([])
         else:
             return (json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy = cls(1, 2, 3)
+        dummy.update(**dictionary)
+        return (dummy)
