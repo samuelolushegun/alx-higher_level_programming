65fc2538375267d5b86a40dba6590a987b9c7e33
Samuel <etchoudinansamuel@gmail.com>
2023-12-13 12:32:26 +0000

    Updt tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,12 +3,27 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
 
-    def test_inst(self):
+    def test_instance_exits(self):
         Rectangle(1, 3)
         Rectangle(5, 7)
         Rectangle(9, 8, 0, 4)
         Rectangle(4, 3, 16, 1, 2)
 
+    def test_area_exists(self):
+        rect = Rectangle(1, 2)
+        self.assertTrue(hasattr(rect, "area"))
+
+    def test_str_exists(self):
+        rect = Rectangle(1, 2)
+        self.assertTrue(hasattr(rect, "__str__"))
+
+    def test_display_exists(self):
+        rect = Rectangle(1, 2)
+        self.assertTrue(hasattr(rect, "display"))
+
+    def test_display_without_x_y(self):
+        rect = Rectangle(1, 2)
+        self.assertIsNone(rect.display())
 
 if __name__ == '__main__':
     unittest.main()
