10e4c9b1df987ff26a69b6d270150f3b05387269
Samuel <etchoudinansamuel@gmail.com>
2023-12-13 08:17:54 +0000

    updt test_base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,10 +4,13 @@ from models.base import Base
 class TestBase(unittest.TestCase):
 
     def test_id_assignment(self):
-        bone = Base()
-        btwo = Base()
-        self.assertEqual(bone.id, 1)
-        self.assertEqual(btwo.id, 2)
+        Base._Base__nb_objects = 0
+        b_1 = Base()
+        b_2 = Base()
+        b_3 = Base()
+        self.assertEqual(b_1.id, 1)
+        self.assertEqual(b_2.id, 2)
+        self.assertEqual(b_3.id, 3)
 
     def test_id_assign_next(self):
         bone = Base()
