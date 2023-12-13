07bda4f97c8f6babe94d6ce345c5f41e3189eef9
Samuel <etchoudinansamuel@gmail.com>
2023-12-13 08:05:40 +0000

    Update test_base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import unittest
 from models.base import Base
 
@@ -7,10 +6,20 @@ class TestBase(unittest.TestCase):
     def test_id_assignment(self):
         bone = Base()
         btwo = Base()
+        self.assertEqual(bone.id, 1)
+        self.assertEqual(btwo.id, 2)
+
+    def test_id_assign_next(self):
+        bone = Base()
+        btwo = Base()
         bthree = Base(13)
         self.assertEqual(bone.id, 1)
         self.assertEqual(btwo.id, 2)
         self.assertEqual(bthree.id, 13)
+
+    def test_id_assign_passed(self):
+        bfour = Base(15)
+        self.assertEqual(bfour.id, 15)
 
 if __name__ == '__main__':
     unittest.main()
