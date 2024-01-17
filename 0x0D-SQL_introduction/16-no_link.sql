6f45b5608c64d8483a95d9a085b4a070027d7250
Samuel <etchoudinansamuel@gmail.com>
2024-01-17 00:45:34 +0000

    Update Task 16 file

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
 -- All records of the table second_table of the database hbtn_0c_0.
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL
+ORDER BY score DESC;
