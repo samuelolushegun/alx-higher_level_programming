9fef37c0715eb106124ee5ca8950044cdded8db9
Samuel <etchoudinansamuel@gmail.com>
2024-01-19 02:22:58 +0000

    ADD TASK 18

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,9 @@
+-- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+SELECT tv_shows.title
+FROM tv_shows
+WHERE id NOT IN (
+      SELECT show_id
+      FROM tv_show_genres
+      JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+      WHERE tv_genres.name = 'Comedy')
+ORDER BY tv_shows.title ASC;
