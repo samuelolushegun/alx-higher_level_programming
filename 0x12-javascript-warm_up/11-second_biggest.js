7951ca4ea143534a1470d63b27bf7158f14d8633
Samuel Olushegun <etchoudinansamuel@gmail.com>
2024-02-13 01:10:06 +0000

    Update in Task 11 file to satisfy semistandard requirement

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,11 +6,11 @@ if (args.length <= 3) {
   let big = 0;
   let big2 = 0;
   for (let i = 2; i < args.length; i++) {
-      if (parseInt(args[i]) >= big) {
+    if (parseInt(args[i]) >= big) {
       big2 = big;
-	  big = parseInt(args[i]);
-      } else if (parseInt(args[i]) > big2 && parseInt(args[i]) !== big) {
-	  big2 = parseInt(args[i]);
+      big = parseInt(args[i]);
+    } else if (parseInt(args[i]) > big2 && parseInt(args[i]) !== big) {
+      big2 = parseInt(args[i]);
     }
   }
   console.log(big2);
