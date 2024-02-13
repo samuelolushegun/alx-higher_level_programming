7564944ecc1dae503e6a14869e13f725ee4bab6d
Samuel Olushegun <etchoudinansamuel@gmail.com>
2024-02-13 16:37:36 +0000

    Add Task 4 file

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let wid = '';
+    for (let i = 0; i < this.width; i++) {
+      wid += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(wid);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
