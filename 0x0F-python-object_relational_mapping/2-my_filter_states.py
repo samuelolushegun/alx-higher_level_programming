af71ca014e74ed9869867cf6b3a619688c9a378b
Samuel Olushegun <etchoudinansamuel@gmail.com>
2024-02-19 08:53:54 +0000

    Add Task 2 file

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''This Module print all matches states with the input'''
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    curs = db.cursor()
+    quer = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    curs.execute(quer, (sys.argv[4],))
+
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+
+    curs.close()
+    db.close()
