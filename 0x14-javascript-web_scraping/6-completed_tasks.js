f1d5516eb0ed62639ffd2ae223f586704622bc55
Samuel <etchoudinan@gmail.com>
2024-03-27 02:55:47 +0000

    Add Task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  const tasks = JSON.parse(body);
+  const completedTasksByUser = {};
+
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (completedTasksByUser[task.userId]) {
+        completedTasksByUser[task.userId]++;
+      } else {
+        completedTasksByUser[task.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasksByUser);
+});
