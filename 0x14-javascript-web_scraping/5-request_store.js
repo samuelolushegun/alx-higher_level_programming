4ed54c03850419af5e87361137f8953be12c07be
Samuel <etchoudinan@gmail.com>
2024-03-27 02:39:04 +0000

    Correction in task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,19 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
+const filePath = process.argv[3];
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
     process.exit(1);
   }
-  const content = JSON.stringify(body);
-  const fs = require('fs');
-  const filePath = process.argv[3];
+
+  const content = body.replace(/^"(.*)"$/, '$1');
+
   fs.writeFile(filePath, content, 'utf-8', (err) => {
     if (err) {
       console.error(err);
+      process.exit(1);
     }
   });
 });
