8ae85b9d655575cac3f009e6b095ecd6bf110cd8
Samuel <etchoudinan@gmail.com>
2024-03-27 01:03:09 +0000

    Add Task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = ('https://swapi-api.alx-tools.com/api/films/' + movieId);
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  const data = JSON.parse(body);
+  console.log(data.title);
+});
